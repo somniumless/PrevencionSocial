71ed36eec96e7ec32c6718a5d04ec9254e24c460
somniumless <carlosperez12104524@gmail.com>
2025-05-20 13:03:03 -0500

    implementación de cambios hechos en el fork y la rama "Carlos" en el main

--- Proyecto_Salud_Mental/app.py
+++ Proyecto_Salud_Mental/app.py
@@ -7,7 +7,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/datos')
+@app.route('/fase1')
+def fase1():
+    return render_template('fase1.html')
+
+@app.route('/fase2')
 def generar_y_mostrar_datos():
     datos_aleatorios = generar_datos_aleatorios() 
     return render_template('fase2.html', datos=datos_aleatorios.to_dict(orient='records'))
